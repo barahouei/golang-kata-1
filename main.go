b4bbb7f02932ef0eaf7d0c099df6cb028fa4dfff
barahouei <abdulghanibarahouei@gmail.com>
2023-10-23 13:47:26 +0330

    feat: add print all

--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	comma         rune   = ';'
+	authorsPath   string = "resources/authors.csv"
+	booksPath     string = "resources/books.csv"
+	magazinesPath string = "resources/magazines.csv"
+)
 
 func main() {
-	fmt.Println(welcomeMessage())
+	var printflag bool
+	flag.BoolVar(&printflag, "print", false, "print all csv files")
+	flag.Parse()
+
+	if printflag {
+		authorsErr := printCSV(authorsPath)
+		if authorsErr != nil {
+			log.Println(authorsErr)
+		}
+
+		fmt.Println(strings.Repeat("*", 10))
+
+		booksErr := printCSV(booksPath)
+		if booksErr != nil {
+			log.Println(booksErr)
+		}
+
+		fmt.Println(strings.Repeat("*", 10))
+
+		magazinesErr := printCSV(magazinesPath)
+		if magazinesErr != nil {
+			log.Println(magazinesErr)
+		}
+
+		fmt.Println(strings.Repeat("*", 10))
+	}
 }
 
-func welcomeMessage() string {
-	return "Hello world!"
+func printCSV(path string) error {
+	file, fileErr := os.Open(path)
+	if fileErr != nil {
+		return fileErr
+	}
+
+	reader := csv.NewReader(file)
+	reader.Comma = comma
+
+	records, readErr := reader.ReadAll()
+	if readErr != nil {
+		return readErr
+	}
+
+	for _, record := range records {
+		fmt.Println(record)
+	}
+
+	return nil
 }
